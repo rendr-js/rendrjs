751500b42836b420629fb5b4c0f6395914fbface
Travis Harmon <harmtrav@gmail.com>
2024-02-09 20:09:54 -0600

    Skip component reconciliation if it has no props

--- src/reconcile.ts
+++ src/reconcile.ts
@@ -31,6 +31,7 @@ export let reconcile = (oldElem: Elem, newElem: Elem): void => {
     }
     if (oldElem.v) {
         newElem.h = oldElem.h;
+        if (!newElem.p && ! oldElem.p) return;
         if (oldElem.m && newElem.m && areDepsEqual(oldElem.m, newElem.m)) {
             newElem.v = oldElem.v;
             return;
